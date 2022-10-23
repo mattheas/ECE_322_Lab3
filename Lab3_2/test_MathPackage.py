39fa88707792bff92605ada552289adb9754212a
mattheas <mattheas@ualberta.ca>
2022-10-22 22:28:53 -0600

    test cases generated that provide statement/branch coverage for MathPackage obj using a Mock obj

--- Lab3_2/test_MathPackage.py
+++ Lab3_2/test_MathPackage.py
@@ -5,26 +5,23 @@ from MathPackage import MathPackage
 
 class TestCoverage(unittest.TestCase):
 
-    # create a mock obj
-    mock_math_obj = Mock()
-    mock_math_obj = MathPackage
+    # create a MathPackage obj and turn it into a mock object
+    myMathPackage = MathPackage()
+    myMathPackage = Mock()
+    
+    # tests provides statement/ branch coverage for methods
 
-    def test_random_method(self):
-        print("hello")
-        # statement coverage should be simple, simply call method w/ parameters
-        # branch coverage is also satisfied since no if() statements
+    def test_random_method_coverage(self):
+        self.myMathPackage.random(3, 1, 5)
+        self.myMathPackage.random.assert_called_once()
+ 
+    def test_max_method_coverage(self):
+        self.myMathPackage.max([1, 2, 3])
+        self.myMathPackage.max.assert_called_once()
 
-    def test_max_method(self):
-        print("hello")
-        # statement coverage should be simple, simply call method w/ parameters
-        # branch coverage is also satisfied since by finding max the if statement should be true at least
-        # once and false at least once IF the list with more than ONE element is passed
-
-    def test_min_method(self):
-        print("hello")
-        # statement coverage should be simple, simply call method w/ parameters
-        # branch coverage is also satisfied since by finding min the if statement should be true at least
-        # once and false at least once IF the list with more than ONE element is passed
+    def test_min_method_coverage(self):
+        self.myMathPackage.min([3, 4, 5])
+        self.myMathPackage.min.assert_called_once()
 
 
 if __name__ == '__main__':
