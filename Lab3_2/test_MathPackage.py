28402f4668fb93d7248dfa17dac12e1e73aead51
mattheas <mattheas@ualberta.ca>
2022-10-23 23:17:46 -0600

    random and max method completely tested per lab specs

--- Lab3_2/test_MathPackage.py
+++ Lab3_2/test_MathPackage.py
@@ -22,7 +22,6 @@ class TestMathPackage(unittest.TestCase):
     # assertion statements
     mock_random.assert_called()
     mock_random.assert_called_once()
-    mock_random.assert_called_once()
     mock_random.assert_called_with(3,1,3)
     mock_random.assert_called_once_with(3,1,3)
     self.assertEqual(mock_random.call_count, 1)
@@ -34,17 +33,38 @@ class TestMathPackage(unittest.TestCase):
     # assertion statements
     expected_args = [((3,1,3),), ((5,1,100),)]
     self.assertEqual(mock_random.call_args_list, expected_args)
-    
-    #expected_calls = ['call.random()', 'call.random()']
-    #self.assertEqual(self.myMathPackage.random.method_calls, expected_calls)
-    
-    print(mock_random.method_calls)
+    self.assertEqual(mock_random.method_calls, [])
     
   @patch.object(MathPackage, 'max')
   def test_max_method(self, mock_max):
     mock_max.return_value = 5
     print("HELLO max")
-    # MOCK A RETURN VALUE HERE
+    
+    mock_max.call_args == ()
+
+    # call to mock method
+    mock_max([3,1,3])
+
+    # assertion statements
+    mock_max.assert_called()
+    mock_max.assert_called_once()
+    mock_max.assert_called_with([3,1,3])
+    mock_max.assert_called_once_with([3,1,3])
+    self.assertEqual(mock_max.call_count, 1)
+    self.assertEqual(mock_max.call_args.args, ([3,1,3],))
+
+    #call to mock method
+    mock_max([5,6,7])
+
+    # assertion statements
+    self.assertEqual(mock_max.call_args_list[0].args, ([3,1,3],))
+    self.assertEqual(mock_max.call_args_list[1].args, ([5,6,7],))
+    self.assertEqual(mock_max.method_calls, [])
+    
+    myMockMathPackage = Mock()
+    myMockMathPackage.max.return_value = 10 
+    self.assertEqual(myMockMathPackage.max(), 10)
+    
     
   @patch.object(MathPackage, 'min')
   def test_min_method(self, mock_min):
