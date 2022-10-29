596717fa105e2c6743113f8d74170237c718f05c
mattheas <mattheas@ualberta.ca>
2022-10-29 14:06:20 -0600

    incorrect assertion and incorrect method call fixed in unit tests

--- Lab3_2/test_coverage_MathPackage.py
+++ Lab3_2/test_coverage_MathPackage.py
@@ -6,15 +6,15 @@ class TestMathPackage(unittest.TestCase):
 
   def test_random_method_coverage(self):
     test_list = MathPackage.random(3, 1, 5)
-    self.assertTrue(len(test_list), 3)
+    self.assertEqual(len(test_list), 3)
 
   def test_max_method_coverage(self):
     max_num = MathPackage.max([1,2,3])
-    self.assertTrue(max_num, 3)
+    self.assertEqual(max_num, 3)
 
   def test_min_method_coverage(self):
-    min_num = MathPackage.max([3,4,5])
-    self.assertTrue(min_num, 3)
+    min_num = MathPackage.min([3,4,5])
+    self.assertEqual(min_num, 3)
     
 
 if __name__ == '__main__':
